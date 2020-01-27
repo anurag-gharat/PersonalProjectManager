fae8c315b33751ae95370df760263865b0752bb1
anurag gharat <anuraggharat55@gmail.com>
2020-01-27 22:24:28 +0530

    "Projects page updated"

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,17 +1,30 @@
 @extends('/layout')
 @section('content')
-<div class="field">
-        <h1 class="title">Projects</h1>
-</div>
-<div class="field">
-        <ol>
-        @foreach($projects as $project)
-        <h3>
-        <li>
-            <a href="/projects/{{$project->id}}">{{$project->name}}</a>
-        </li>
-        </h3>
-        @endforeach
-    </ol>
+
+<div class="container p-4 bg-light shadow-lg pl-5 h-75">
+    <div class="title-display">
+    <h1 class="title">Active Projects:</h1>
+    </div>
+    <div class="row list-holder pt-3">
+        <div class="col-md-6 pl-5">
+            <ol class="projects-list">
+            @foreach($projects as $project)
+            <h3>
+            <li class="projects-list-item">
+            <a href="/projects/{{$project->id}}" class="color">{{$project->name}}</a>
+            </li>
+            </h3>
+            @endforeach
+            </ol>
+        </div>
+        <div class="col-md-6">
+            <div class="display bg-dark w-100 h-100">
+
+            </div>
+
+        </div>
+        
+    </div>
+
 </div>    
 @endsection
